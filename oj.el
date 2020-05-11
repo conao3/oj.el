9fbfad42d6b44ce0f38c26baebb70d7a2d68d0c4
conao3 <conao3@gmail.com>
2020-05-12 01:41:20 +0900

    fix oj-install-package

--- oj.el
+++ oj.el
@@ -56,17 +56,6 @@
   (with-current-buffer oj-buffer
     (comint-send-input)))
 
-(defun oj--install-package-maybe ()
-  "Install `oj' pip package via `pip3'."
-  (unless (executable-find "oj")
-    (unless (yes-or-no-p "Install online-judge-tools via pip3?")
-      (error "Abort install"))
-    (unless (executable-find "python3")
-      (error "Missing `python3.'  Please ensure Emacs's PATH and the installing"))
-    (unless (executable-find "pip3")
-      (error "Missing `pip3'.  Please ensure Emacs's PATH and the installing"))
-  (oj--exec-script "pip3 install online-judge-tools")))
-
 
 ;;; Main
 
@@ -78,8 +67,19 @@
     (with-current-buffer oj-buffer
       (setq comint-process-echoes t))
     (make-comint-in-buffer "oj" oj-buffer shell-file-name))
-  (display-buffer oj-buffer)
-  (oj--install-package-maybe))
+  (display-buffer oj-buffer))
+
+(defun oj-install-package ()
+  "Install `oj' pip package via `pip3'."
+  (interactive)
+  (unless (executable-find "oj")
+    (unless (yes-or-no-p "Install online-judge-tools via pip3?")
+      (error "Abort install"))
+    (unless (executable-find "python3")
+      (error "Missing `python3.'  Please ensure Emacs's PATH and the installing"))
+    (unless (executable-find "pip3")
+      (error "Missing `pip3'.  Please ensure Emacs's PATH and the installing"))
+  (oj--exec-script "pip3 install online-judge-tools")))
 
 (defun oj-login ()
   "Login online-judge system."
