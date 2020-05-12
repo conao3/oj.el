3f443a82579898b7f721385bdf8ce92530141b56
conao3 <conao3@gmail.com>
2020-05-12 17:22:24 +0900

    require quickrun

--- oj.el
+++ oj.el
@@ -5,7 +5,7 @@
 ;; Author: Naoya Yamashita <conao3@gmail.com>
 ;; Version: 0.0.1
 ;; Keywords: convenience
-;; Package-Requires: ((emacs "26.1"))
+;; Package-Requires: ((emacs "26.1") (quickrun "2.2"))
 ;; URL: https://github.com/conao3/oj.el
 
 ;; This program is free software: you can redistribute it and/or modify
@@ -27,6 +27,8 @@
 
 
 ;;; Code:
+
+(require 'quickrun)
 
 (defgroup oj nil
   "Competitive programming tools client."
