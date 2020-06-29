b1559b97073539587b1bb0495c8716b7210de8e4
conao3 <conao3@gmail.com>
2020-06-30 06:23:27 +0900

    tagged v1.0.5

--- oj.el
+++ oj.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2020  Naoya Yamashita
 
 ;; Author: Naoya Yamashita <conao3@gmail.com>
-;; Version: 1.0.4
+;; Version: 1.0.5
 ;; Keywords: convenience
 ;; Package-Requires: ((emacs "26.1") (quickrun "2.2"))
 ;; URL: https://github.com/conao3/oj.el
